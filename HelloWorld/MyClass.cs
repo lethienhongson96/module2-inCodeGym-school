4776e3e229ad9b9a88f5e88c2786b9c9bc2860ae
lethienhongson96 <lethienhongson96@gmail.com>
2020-07-02 14:51:32 +0700

    my-first-in-C#

---
+++ HelloWorld/MyClass.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace HelloWorld
+{
+    public class MyClass
+    {
+        public string ReturnMessage()
+        {
+            return "happy code";
+        }
+
+    }
+}
---
+++ HelloWorld/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.WriteLine("enter a number: ");
+            number = Convert.ToInt32(Console.ReadLine());
+            if (number < 2)
+            {
+                Console.WriteLine(number + " is not a number");
+            }
+            else
+            {
+                int i = 2;
+                bool check = true;
+                string result = "";
+                while (i <= Math.Sqrt(number))
+                {
+                    if (number % i == 0)
+                    {
+                        check = false;
+                        break;
+                    }
+                    i++;
+                }
+                if (check)
+                {
+                    result = number + " is a prime";
+                }
+                else
+                {
+                    result = number + " is not a prime";
+                }
+                Console.WriteLine(result);
+            }
+
+        }
+    }
+}
